f213f0c3c0317419abd83473708579067088f975
Doniyor643 <doniyorotashov92@gmail.com>
2022-02-18 11:35:59 +0500

    master

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -12,134 +11,280 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
 
-  bool isLogin = false;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('assets/images/party.jpg'),
-              fit: BoxFit.cover
-            )
-          ),
-          child: Container(
-            padding: const EdgeInsets.all(20),
-            width: double.infinity,
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.topCenter,
-                colors: [
-                  Colors.black.withOpacity(0.8),
-                  Colors.black.withOpacity(0.5),
-                  Colors.black.withOpacity(0.4),
-                  Colors.black.withOpacity(0.2),
-                ]
-              )
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: const Color(0xFF263b55),
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+            const SizedBox(
+              width: 210,
+              child: Image(image: AssetImage('assets/images/amazon_logo.jpg'),
+                height: 50,
+                fit: BoxFit.cover,),
             ),
-            child: Column(
-              children: [
-                const SizedBox(height: 30,),
-                Column(children: const [
-                  Text("Welcome to HiTech party",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 30,
-                        fontWeight: FontWeight.bold
-                    ),
-                  ),
-                  SizedBox(height: 20,),
-                  Text("Your action is your opportunity",
-                    style: TextStyle(
-                        color: Colors.grey,
-                        fontSize: 20,
-                        fontWeight: FontWeight.w300
-                    ),
-                  ),
-                  SizedBox(height: 550,),
-                ],),
-
-
-                isLogin ?
-
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
+              SizedBox(
+                width: 100,
+                child: Row(
                   children: [
-                    Container(
-                      height: 50,
-                      decoration: BoxDecoration(
-                        gradient: LinearGradient(
-                            begin: Alignment.topRight,
-                            colors: [
-                              Colors.deepOrange.withOpacity(0.8),
-                              Colors.deepOrange.withOpacity(0.6),
-                              Colors.deepOrange.withOpacity(0.4),
-                              Colors.deepOrange.withOpacity(0.2),
-                            ]
-                        ),
-                        borderRadius: BorderRadius.circular(50),
-                        color: Colors.deepOrange,
-                      ),
-                      child: const Center(
-                        child: Text("Start Up", style: TextStyle(color: Colors.white),),
-                      ),
-                    ),
+                    IconButton(
+                        onPressed: (){},
+                        icon: const Icon(Icons.mic, color: Colors.white,)),
+                    IconButton(
+                        onPressed: (){},
+                        icon: const Icon(Icons.shopping_cart, color: Colors.white,)),
                   ],
                 )
-
-                :
-
-                Row(
-
-                  children: [
-                    Expanded(
-                      child: Container(
-                        height: 50,
-                        decoration: BoxDecoration(
-                          gradient: LinearGradient(
-                              begin: Alignment.topRight,
-                              colors: [
-                                Colors.red.withOpacity(0.8),
-                                Colors.red.withOpacity(0.6),
-                                Colors.red.withOpacity(0.4),
-                                Colors.red.withOpacity(0.2),
-                              ]
-                          ),
-                          borderRadius: BorderRadius.circular(50),
-                          color: Colors.red,
-                        ),
-                        child: const Center(
-                          child: Text("Google+", style: TextStyle(color: Colors.white),),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(width: 20,),
-                    Expanded(
-                      child:Container(
-                        height: 50,
-                        decoration: BoxDecoration(
-                          gradient: LinearGradient(
-                            begin: Alignment.topRight,
-                            colors: [
-                              Colors.blue.withOpacity(0.8),
-                              Colors.blue.withOpacity(0.6),
-                              Colors.blue.withOpacity(0.4),
-                              Colors.blue.withOpacity(0.2),
-                            ]
-                          ),
-                          borderRadius: BorderRadius.circular(50),
-                          color: Colors.blue,
-                          ),
-                        child: const Center(
-                          child: Text("Facebook+", style: TextStyle(color: Colors.white),),
-                        ),
-                    ),)
-                  ],
-                )
-            ],),
-          ),
+              )
+          ],),
         ),
+      drawer: const Drawer(),
+      body: Container(
+        color: Colors.grey,
+        child: Column(children: [
+          // Search
+          Container(
+            padding: const EdgeInsets.only(left: 10, right: 10, bottom: 10),
+            color: const Color(0xFF263b55),
+            child: Container(
+              height: 50,
+              padding: const EdgeInsets.only(left: 10,right: 10),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                border: Border.all(color: Colors.grey, width: 1),
+              ),
+              child: Row(
+                children: const [
+                  Expanded(
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Search',
+                          icon: Icon(Icons.search),
+                        ),
+                      )
+                  ),
+                  Icon(Icons.camera_alt, color: Color(0xFF263b55),)
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+              child: ListView(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.only(left: 10,right: 10),
+                    color: Colors.blueGrey,
+                    height: 45,
+                    child: Row(children: const [
+                      Icon(Icons.location_on,color: Colors.white,),
+                      SizedBox(height: 5,),
+                      Text('Republic of Uzbekistan',style: TextStyle(color: Colors.white),)
+                    ],),
+                  ),
+                  // adds
+                  SizedBox(
+                    height: 140,
+                    child: Row(children: [
+                      Expanded(
+                          child: Container(
+                            color: Colors.white,
+                            child: Container(
+                              decoration: const BoxDecoration(
+                                borderRadius: BorderRadius.only(
+                                  topRight: Radius.circular(70),
+                                  bottomRight: Radius.circular(70),
+                                ),
+                                color: Colors.white,
+                                image: DecorationImage(
+                                  image: AssetImage('assets/images/progMobile.jpg'),
+                                  fit: BoxFit.cover,
+                                )
+                              ),
+                        ),
+                          )
+                      ),
+                      Container(
+                        padding: const EdgeInsets.all(20),
+                        color: Colors.white,
+                        child: const Center(child: Text('We ship 45million products'),),
+                      ),
+                    ],),
+                  ),
+                  const SizedBox(height: 8,),
+                  // Sign in
+                  Container(
+                    height: 160,
+                    padding: const EdgeInsets.only(left: 10,right: 10),
+                    color: Colors.white,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text('Your action is your opportunity'),
+                        const SizedBox(height: 15,),
+
+                        Container(
+                          height: 50,
+                          width: double.infinity,
+                          color: Colors.deepOrange,
+                          child: const Center(
+                              child: Text("Sign in",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 16),
+                              )
+                          ),
+                        ),
+                        const SizedBox(height: 15,),
+                        const Text('Create an account',style: TextStyle(fontSize: 16, color: Colors.blue),),
+
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 8,),
+                  //Deal
+                  Container(
+                    padding: const EdgeInsets.all(16),
+                    color: Colors.white,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Text('Deal of the day',style: TextStyle(fontSize: 22),),
+                        Image(
+                          height: 240,
+                          width: double.infinity,
+                          image: AssetImage('assets/images/work.jpg'),
+                          fit: BoxFit.cover,
+                        ),
+                        SizedBox(height: 8,),
+                        Text('Good work good result',style: TextStyle(fontSize: 16),),
+                        SizedBox(height: 8,),
+                        Text('\$300.0 - \$500.0',style: TextStyle(fontSize: 16),)
+
+                    ],),
+                  ),
+                  const SizedBox(height: 8,),
+                  //Best
+                  Container(
+                    padding: EdgeInsets.all(16),
+                    color: Colors.white,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                      const Text("Best in electronics",style: TextStyle(fontSize: 22),),
+                      const SizedBox(height: 16,),
+                      SizedBox(
+                        height: MediaQuery.of(context).size.width,
+                        child: Row(
+                          children: [
+                            Expanded(
+                              child: Column(
+                                children: const [
+                                  Expanded(
+                                    child: Image(
+                                      image: AssetImage('assets/images/programming1.jpg'),
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
+                                  SizedBox(height: 8,),
+                                  Expanded(
+                                    child: Image(
+                                      image: AssetImage('assets/images/programming2.jpg'),
+                                      fit: BoxFit.cover,
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+                            const SizedBox(width: 8,),
+                            Expanded(
+                              child: Column(
+                                children: const [
+                                  Expanded(
+                                    child: Image(
+                                      image: AssetImage('assets/images/programming3.jpg'),
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
+                                  SizedBox(height: 8,),
+                                  Expanded(
+                                    child: Image(
+                                      image: AssetImage('assets/images/programming4.jpg'),
+                                      fit: BoxFit.cover,
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+                        ],),
+                      )
+                    ],),
+                  ),
+                  const SizedBox(height: 8,),
+
+                  //Home work
+                  Container(
+                    padding: const EdgeInsets.all(16),
+                    color: Colors.white,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text("Home work",style: TextStyle(fontSize: 22),),
+                        const SizedBox(height: 16,),
+                        Container(
+                          color: Colors.white,
+                          height: MediaQuery.of(context).size.width,
+                          child: Column(children: [
+                            Expanded(
+                              child: Container(
+                                color: Colors.blue,
+                                width: MediaQuery.of(context).size.width,
+                                child: const Image(
+                                  image: AssetImage('assets/images/programming4.jpg'),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(height: 8,),
+                            Expanded(child: Row(children: [
+                              Expanded(
+                                child: Container(
+                                  height: MediaQuery.of(context).size.width,
+                                  color: Colors.red,
+                                  child: const Image(
+                                    image: AssetImage('assets/images/programming1.jpg'),
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(width: 8,),
+                              Expanded(child: Container(
+                                height: MediaQuery.of(context).size.width,
+                                color: Colors.green,
+                                child: const Image(
+                                  image: AssetImage('assets/images/programming3.jpg'),
+                                  fit: BoxFit.cover,
+                                ),
+                                ),
+                              )
+                            ],)
+                            )
+                          ],
+                          ),
+                        )
+                      ],),
+                  ),
+
+
+                  const SizedBox(height: 8,),
+                ],
+              ))
+        ],),
+      ),
         );
   }
 }
